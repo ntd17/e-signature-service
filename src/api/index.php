9a286d3d3dc60a1b9f8268694652d3597b4b54fa
Joao Natividade <joao.natividade01@etec.sp.gov.br>
2025-06-20 13:13:04 -0300

    Update index.php

--- src/api/index.php
+++ src/api/index.php
@@ -35,7 +35,7 @@ if (!in_array($apiKey, $validApiKeys)) {
     echo json_encode(['error' => 'Invalid API key']);
     exit;
 }
-
+*/
 $method = $_SERVER['REQUEST_METHOD'];
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $pathParts = explode('/', trim($path, '/'));
